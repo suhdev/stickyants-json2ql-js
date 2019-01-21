b3a2da7b8e0e1ff6fa7a6d935571f896c624b386
Suhail Abood <suhail.abood@gmail.com>
2019-01-21 20:24:58 +0000

    Adds SqlSort.ts tests

---
+++ __tests__/SqlSortSpec.ts
@@ -0,0 +1,46 @@
+import { SqlSort } from '../src/SqlSort';
+import { SqlSorterDirection, SqlOperator, SqlRefinerType } from '../src/enums';
+describe('Testing SqlSort', () => {
+  it('it should set direction to ascending', () => {
+    const field = 'FirstName';
+    const sort = new SqlSort(null, field).asc;
+    const result = sort.toJSON();
+    expect(result.key).toEqual(field);
+    expect(result.type).toEqual(SqlRefinerType.Sort);
+    expect(result.value).toEqual(SqlSorterDirection.ASC);
+  });
+
+  it('it should set direction to descending', () => {
+    const field = 'FirstName';
+    const sort = new SqlSort(null, field).desc;
+    const result = sort.toJSON();
+    expect(result.key).toEqual(field);
+    expect(result.type).toEqual(SqlRefinerType.Sort);
+    expect(result.value).toEqual(SqlSorterDirection.DESC);
+  });
+
+  it('it should set the key', () => {
+    const field = 'FirstName';
+    const key = 'LastName';
+    const sort = new SqlSort(null, field).desc.key(key);
+    const result = sort.toJSON();
+    expect(result.key).toEqual(key);
+    expect(result.type).toEqual(SqlRefinerType.Sort);
+    expect(result.value).toEqual(SqlSorterDirection.DESC);
+  });
+
+  it('it should construct object from json', () => {
+    const field = 'FirstName';
+    const key = 'LastName';
+    const sort = SqlSort.fromJson({
+      key,
+      value:SqlSorterDirection.ASC,
+      operator:SqlOperator.EQ,
+      type:SqlRefinerType.Sort,
+    });
+    const result = sort.toJSON();
+    expect(result.key).toEqual(key);
+    expect(result.type).toEqual(SqlRefinerType.Sort);
+    expect(result.value).toEqual(SqlSorterDirection.ASC);
+  });
+});
--- src/SqlSort.ts
+++ src/SqlSort.ts
@@ -1,41 +1,95 @@
 import { SqlSorterDirection, SqlRefinerType, SqlOperator } from './enums';
 import { SqlQueryModel } from './SqlQueryModel';
+import { ISqlRefiner } from './ISqlRefiner';
 
+/**
+ * Represents a sort option in a SELECT statement
+ */
 export class SqlSort {
-  private key:string;
-  private direction:SqlSorterDirection;
-  context: SqlQueryModel;
+  private $key:string;
+  private $direction:SqlSorterDirection;
+  private $context: SqlQueryModel;
 
   constructor(context:SqlQueryModel, key:string) {
-    this.context = context;
-    this.key = key;
+    this.$context = context;
+    this.$key = key;
   }
 
+  /**
+   * Sets the sort direction to be ascending
+   */
   get asc() {
-    this.direction = SqlSorterDirection.ASC;
+    this.$direction = SqlSorterDirection.ASC;
     return this;
   }
 
+
+  /**
+   * Sets the sort direction to be descending 
+   */
   get desc() {
-    this.direction = SqlSorterDirection.DESC;
+    this.$direction = SqlSorterDirection.DESC;
     return this;
   }
 
+  /**
+   * Sets the column name to sort by
+   * @param k the column name
+   */
+  key(k:string) {
+    this.$key = k;
+    return this;
+  }
+
+  /**
+   * Sets the context model 
+   * @param ctx the context to use the sort on
+   */
+  context(ctx:SqlQueryModel){
+    this.$context = ctx; 
+    return this; 
+  }
+
+  /**
+   * Adds the sort option to the context model 
+   * and return the context 
+   */
   add() {
-    this.context.addSort({
+    this.$context.addSort({
       type:SqlRefinerType.Sort,
-      value:this.direction,
-      key:this.key,
+      value:this.$direction,
+      key:this.$key,
       operator:SqlOperator.EQ,
     });
-    return this.context;
+    return this.$context;
   }
 
-  toSqlRefiner() {
+  /**
+   * Constructs a new sort option model using a JSON object
+   * @param obj the json object to construct the sort option from
+   */
+  static fromJson(obj:ISqlRefiner) {
+    const s = new SqlSort(null, obj.key);
+    if (obj.value === SqlSorterDirection.ASC) {
+      s.asc;
+    }else {
+      s.desc;
+    }
+    return s;
+  }
+
+  toJSON():ISqlRefiner {
+    return this.toSqlRefiner();
+  }
+
+  /**
+   * Extracts the SQL refiner definition
+   */
+  toSqlRefiner():ISqlRefiner {
     return {
       type:SqlRefinerType.Sort,
-      value:this.direction,
-      key:this.key,
+      value:this.$direction,
+      key:this.$key,
       operator:SqlOperator.EQ,
     };
   }
